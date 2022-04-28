d752440bb7c2003315139b917dc05445cc88644d
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-28 06:30:56 -0400

    commit tree cbd77ae0f8c1288fe71460b457f547ab515f3bdb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(driverNames, filteredName) {
+    return driverNames.filter((matchedName) => matchedName.toLowerCase() === filteredName.toLowerCase());
+}
+  
+function fuzzyMatch(driverNames, inputString) {
+    return driverNames.filter((matchedName) => matchedName.toLowerCase().indexOf(inputString.toLowerCase()) === 0 );
+}
+  
+function matchName(driverObjArray, inputName) {
+    return driverObjArray.filter((obj) => obj.name === inputName);
+}
